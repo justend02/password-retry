47462fe29cd2a125d229eb8112d202cbb1e23974
Jerry Wang <kuanchieh.w@gmail.com>
2021-09-06 08:16:45 +0900

    version 3

--- password-retry.py
+++ password-retry.py
@@ -1,11 +1,15 @@
 password = 'a123456'
 i = 3
 while i > 0:
+	i = i - 1
 	pwd = input('please enter your password: ')
 	if pwd ==password:
 		print('you are right')
 		break
 	else:
-		i = i - 1
-		print('you are wrong', i, 'times' )
+		print('you are wrong')
+		if i > 0:
+			print('still', i, 'times')
+		else:
+			print('you are going to lose your account')
 
